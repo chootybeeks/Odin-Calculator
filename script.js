205e1398b0a1f22bd4845d414e96587d9ef0221a
chootybeeks <quadi.d.banks@gmail.com>
2022-07-16 14:36:54 -0400

    Create basic calculator functions and operate function

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+//I'm confused by this function, leave it for now but probably will have to refactor it
+function operate(operator, a, b) {
+    return operator(a, b)
+}
