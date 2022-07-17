55f74ace69e76614a27bc85bbf7abbe5352b3c7a
chootybeeks <quadi.d.banks@gmail.com>
2022-07-17 10:05:14 -0400

    Add calculator class

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+}
+
 //Selectors for all of the buttons and operations
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
