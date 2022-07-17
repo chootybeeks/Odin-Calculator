3aa90bd723d26c5d60987184e42aab2a8225aea7
chootybeeks <quadi.d.banks@gmail.com>
2022-07-17 10:07:33 -0400

    Add all of the calculator functions

--- script.js
+++ script.js
@@ -14,3 +14,27 @@ const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+clear() {
+
+}
+
+delete() {
+
+}
+
+appendNumber(number) {
+
+}
+
+chooseOperation(operation) {
+
+}
+
+compute() {
+
+}
+
+updateDisplay() {
+    
+}
