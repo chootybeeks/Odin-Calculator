b6dcdf0649f54269ae1a55ebf414be2d248fdbe7
chootybeeks <quadi.d.banks@gmail.com>
2022-07-17 11:14:38 -0400

    Implement getDisplayNumber helper function

--- script.js
+++ script.js
@@ -63,6 +63,26 @@ class Calculator {
 
     }
 
+    //Helper function to add comma delimiters
+    getDisplayNumber(number) {
+        const stringNumber = number.toString();
+        const integerDigits = parseFloat(stringNumber.split(',')[0]);
+        const decimalDigits = stringNumber.split('.')[1];
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = '';
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {
+            maximumFractionDigits: 0
+            });
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`;
+        } else {
+            return integerDisplay;
+        }
+    }
+
     updateDisplay() {
     //This is for testing!//
     //this.currentOperandTextElement.innerText = this.currentOperand
